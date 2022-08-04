9a9c127992072c9cf1c37ee1b964cdd414b27cc8
granger kenolane <kenolane28@gmail.com>
2022-08-04 22:01:44 +0200

    Add title acceuil

--- portfolio/src/pages/Acceuil.js
+++ portfolio/src/pages/Acceuil.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Header from "../components/Header";
 import Cards from "../components/Cards";
 import Footer from "../components/Footer";
 
 
 const Acceuil = () => {
+
+    useEffect(() => {
+        document.title = "Acceuil"
+    }, []);
 
     let data = [
         {
