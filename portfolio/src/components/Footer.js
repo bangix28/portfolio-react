4ea44f7689a23850f64dcc72f07a0f65467c6d40
granger kenolane <kenolane28@gmail.com>
2022-08-04 21:21:48 +0200

    Create footer components

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+
+const Footer = () => {
+    return (
+        <div className="header">
+            <hr className={"header-divider"} />
+            <div className={"footer"}>
+                <div className={"footer-text"}>
+                    <p>Copyright © 2022 Kenolane Granger</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Footer
