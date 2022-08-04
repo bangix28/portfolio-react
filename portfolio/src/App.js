29aaf899e85717a907f040896a813d6489fa5946
granger kenolane <kenolane28@gmail.com>
2022-08-04 23:48:40 +0200

    Add 404 Pages and article with parameter

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Acceuil from "./pages/Acceuil";
+import Article from "./pages/Article";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
     return (
     <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />}/>
-                <Route path="/acceuil" element={<Acceuil />}/>
+                <Route path="*" element={<NotFound/>} />
+                <Route path="/">
+                    <Route path="" element={<Home />}/>
+                    <Route path="acceuil" element={<Acceuil />}/>
+                    <Route path="article/:article" element={<Article />}/>
+                </Route>
             </Routes>
         </BrowserRouter>
     )
