5333a005ee07d030af269ced44e4a669dbf2d280
granger kenolane <kenolane28@gmail.com>
2022-08-04 23:49:01 +0200

    Create pages Article for the article

---
+++ portfolio/src/pages/Article.js
@@ -0,0 +1,41 @@
+import Header from "../components/Header";
+import React from "react";
+import Footer from "../components/Footer";
+import {useParams} from "react-router-dom";
+
+
+const Article = () => {
+
+    let articleId = useParams();
+    console.log(articleId);
+
+    let data = [
+        {
+            title: "Web Development",
+            text: "I am a web developer with a passion for building web applications. I have a background in computer science and I am currently working on my own projects.",
+            image: "https://dummyimage.com/450x350/fff/aaa\\n"
+        }
+        ]
+
+    return (
+        <div className={"page-background"}>
+            <Header/>
+            <div className={"container"}>
+                <article className={"article"}>
+                    <img src={data.image} alt={"image d'article"}/>
+                    <div className={"article-title"}>
+                        <h3>{data.title}</h3>
+                    </div>
+                    <div className={"article-text"}>
+                        <p>{data.text}</p>
+                    </div>
+
+                </article>
+            </div>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Article;
+
