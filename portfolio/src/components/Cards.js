82b2a7166d2b1a1b19196a88e5610c80db3d730f
granger kenolane <kenolane28@gmail.com>
2022-08-15 15:40:06 +0200

    Add firebase for dynamical cards

--- portfolio/src/components/Cards.js
+++ portfolio/src/components/Cards.js
@@ -1,24 +1,36 @@
 import * as React from "react";
 import {Image} from "react-bootstrap";
+import {storage} from "../firebase.config";
+import {useEffect, useState} from "react";
 
-// importing material UI components
 
 const Cards = ({card}) => {
+
+       const [image, setImage] = useState(null);
+useEffect(() => {
+    storage.ref(card.image).getDownloadURL().then(url => {
+        setImage(url);
+    }).catch(error => {
+        console.log(error);
+    });
+});
 
     return (
             <article className={"card"}>
                 <div className={"card-image"}>
-                    <a href={"#"}>
-                        <Image className={"card-image-responsive"} src={card.image} alt=""/>
+                    <a href={"/article/"+ card.id}>
+                        <Image className={"card-image-responsive"} src={image} alt=""/>
                     </a>
                 </div>
                 <div className={"card-content"}>
+                    <a href={"#"}>
                     <div className={"card-title"}>
                         <h3>{card.title.substring(0, 45)}</h3>
                     </div>
                     <div className={"card-text"}>
-                        <p>{card.text.substring(0, 185)}</p>
+                        <p>{card.content.substring(0, 185)}</p>
                     </div>
+                    </a>
                 </div>
             </article>
     );
